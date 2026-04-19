5d25f5959b7bdcf3b142cc788d3db1957bb1e91c
facto974 <123896139+facto974@users.noreply.github.com>
2026-04-19 10:01:13 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ echo "📦 Installing frontend dependencies..."
 cd frontend
 yarn install
 
+yarn add date-fns@^3.0.0
+yarn add @babel/core@^7.0.0
+yarn add react-is@^18.0.0
+yarn add --dev @types/node
+
 echo "🔨 Building frontend..."
 REACT_APP_BACKEND_URL="" yarn build
 
